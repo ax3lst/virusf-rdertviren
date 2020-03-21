8b62b1e6dcd725e12d211fc250224b6194c45fc0
Axel Solis Trompler <ax3lst@gmail.com>
2020-03-21 21:04:39 +0100

    animations

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../styles/App.css';
 
 import Questionaire from './Questionaire'
 
-function App() {
-  return (
-    <div className="App">
-      <nav>
-        <img src={process.env.PUBLIC_URL+'logo.jpg'} alt=""/>
-      </nav>
-      <div className="header">
-        <h1>Leitfragen beim Homeoffice</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi, ea ducimus sapiente 
-          dicta tempore quaerat possimus eos illo quod itaque<br/>consequuntur quos, sunt corporis, 
-          repellat blanditiis! Est ex nihil possimus.</p>
+class App extends Component {
+
+  state = {
+    headerVisible: true
+  }
+
+  setHeader = (bool) => {
+    this.setState({headerVisible: bool});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <img src={process.env.PUBLIC_URL+'logo.jpg'} alt=""/>
+        </nav>
+        <div className={`header ${this.state.headerVisible ? "" : "headerInv"}`}>
+          <h1>Leitfragen beim Homeoffice</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi, ea ducimus sapiente 
+            dicta tempore quaerat possimus eos illo quod itaque<br/>consequuntur quos, sunt corporis, 
+            repellat blanditiis! Est ex nihil possimus.</p>
+        </div>
+        <div className="content">
+          <Questionaire setHeader={this.setHeader} />
+        </div>
+        <footer>
+          <div className="footer-links">
+            <a href="#">Impressum</a>
+            <a href="#">Datenschutz</a>
+          </div>
+        </footer>
       </div>
-      <div className="content">
-        <Questionaire />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Questionaire.js
+++ src/components/Questionaire.js
@@ -5,33 +5,41 @@ import '../styles/Questionaire.css'
 
 export default class Questionaire extends Component {
     state = {
-        started: true
+        started: true,
+        stakeholder: 'Test',
+    }
+
+    stakeholderClicked = (stakeholder) => {
+        console.log(stakeholder)
+        this.setState({stakeholder: stakeholder, started: false})
+        this.props.setHeader(false);
     }
 
     getAwnsers = () => {
         return [
-            {text: "User", empfehlung: [], optional: "optionale beschreibung", next: [1]},
-            {text: "Manager", empfehlung: [], optional: "optionale beschreibung", next: [2]},
-            {text: "Admin", empfehlung: [], optional: "optionale beschreibung", next: [3]},
+            {text: "Gute Laune", empfehlung: [], optional: "optionale beschreibung", next: [1]},
+            {text: "Singen", empfehlung: [], optional: "optionale beschreibung", next: [2]},
+            {text: "Tanzen", empfehlung: [], optional: "optionale beschreibung", next: [3]},
         ]
     }
     render() {
         if (this.state.started) {
             return (
                 <div className="question">
-                    <h2 className="title">Zum Start: <i>Welche Rolle haben Sie?</i></h2>
+                    <h2 className="title"><i>Welche Rolle haben Sie?</i> 
+                    </h2>
                     <div className="question-start">
                         <div>
                             <img src={process.env.PUBLIC_URL+'/shareholder/image2.jpg'} alt=""/>
-                            <button>User:in</button>
+                            <button onClick={(e) => this.stakeholderClicked('User:in')}>User:in</button>
                         </div>
                         <div>
                             <img src={process.env.PUBLIC_URL+'/shareholder/image0.jpg'} alt=""/>
-                            <button>Manager:in</button>
+                            <button onClick={(e) => this.stakeholderClicked('Manager:in')}>Manager:in</button>
                         </div>
                         <div>
                             <img src={process.env.PUBLIC_URL+'/shareholder/image1.jpg'} alt=""/>
-                            <button>Administrator:in</button>
+                            <button onClick={(e) => this.stakeholderClicked('Administrator:in')}>Administrator:in</button>
                         </div>
                     </div>
                 </div>
